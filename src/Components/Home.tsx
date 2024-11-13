d4a11a47e8d24c8125c3c23dcf696e3318d41e3e
Achyut <onestring22@gmail.com>
2024-11-13 14:05:58 +0530

    HOME page added

---
+++ src/Components/Home.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+function HOMEPage() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center">
+        <h1>
+            HOMEPage
+            <Link href={"/login"}>Login</Link>
+        </h1>
+    </div>
+  )
+}
+
+export default HOMEPage
